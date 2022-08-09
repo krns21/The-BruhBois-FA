ac04d113a7214ea41c58d0a8b617d9437d15e0d1
krns21 <krns.0021@gmail.com>
2022-08-09 02:44:15 +0100

    v1.2 [Fix minor bugs]

--- The-BruhBois-FA/src/app/app-routing.module.ts
+++ The-BruhBois-FA/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CountrylistComponent } from './countrylist/countrylist.component';
 
 const routes: Routes = [
-  {path:'countrylist', component: CountrylistComponent}
 ];
 
 @NgModule({
