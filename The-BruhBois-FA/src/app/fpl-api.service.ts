404481cf1028056fc3012d6daf9ffc91bcee1c2c
krns21 <krns.0021@gmail.com>
2022-08-09 02:40:02 +0100

    Update v1.1

---
+++ The-BruhBois-FA/src/app/fpl-api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FplApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  /** GET premier league standings*/
+  
+
+}
